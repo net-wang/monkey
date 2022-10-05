54596b8848e29fb6ac25959b1932de9e8d469e5a
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-10-05 14:30:31 -0400

    Island: Add utils.get_or_create_target_machine()
    Both ScanEventHandler and update_nodes_on_exploitation() will need this
    functionality. Extracting it some place common.
    
    I didn't put it into the MachineRepository because the semantics of
    creating a machine if not found are likely specific to a small set of
    use cases, rather than part of the general interface.

--- monkey/monkey_island/cc/agent_event_handlers/__init__.py
+++ monkey/monkey_island/cc/agent_event_handlers/__init__.py
@@ -1,3 +1,4 @@
 from .save_event_to_event_repository import save_event_to_event_repository
 from .save_stolen_credentials_to_repository import save_stolen_credentials_to_repository
 from .scan_event_handler import ScanEventHandler
+from .update_nodes_on_exploitation import update_nodes_on_exploitation
