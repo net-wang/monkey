6caca1496bc477d526940b9305f5b28b3be05f4a
vakarisz <vakarisz@yahoo.com>
2022-10-14 14:02:40 +0300

    Auto registration prototype

--- monkey/tests/unit_tests/common/agent_events/test_agent_event_registry.py
+++ monkey/tests/unit_tests/common/agent_events/test_agent_event_registry.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock
+
 import pytest
 from pydantic import Field
 
@@ -12,6 +14,38 @@ def test_reject_invalid_classes():
 
     with pytest.raises(TypeError):
         registry.register(T)
+
+
+def metaclass_factory(registry):
+    class RegistratorMeta(type):
+        def __new__(cls, name, bases, dct):
+            x = super().__new__(cls, name, bases, dct)
+            print(f"We call {registry} with {name} to register {x}")
+            registry(name, x)
+            return x
+    return RegistratorMeta
+
+
+def test_registration_2():
+    reg1 = MagicMock()
+    reg2 = MagicMock()
+
+    meta1 = metaclass_factory(reg1)
+    meta2 = metaclass_factory(reg2)
+
+    class SomeEvent(metaclass=meta1):
+        some_param: int = Field(default=435)
+
+    class OtherEvent(metaclass=meta2):
+        other_param: float = Field(default=123.456)
+
+    call_args = reg1.call_args[0]
+    assert call_args[0] == "SomeEvent"
+    assert call_args[1] == SomeEvent
+
+    call_args = reg2.call_args[0]
+    assert call_args[0] == "OtherEvent"
+    assert call_args[1] == OtherEvent
 
 
 def test_registration():
